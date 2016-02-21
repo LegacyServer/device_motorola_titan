5f65122933987441c2d6e4e4d444faa62b980235
Vatsal <koradiavatsal@hotmail.com>
2016-02-21 16:14:13 +0100

    IO-ify

--- io.mk
+++ io.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/io/common.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := aosp_titan
+PRODUCT_NAME := io_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo aosp_titan-userdebug
-add_lunch_combo aosp_titan-user
-add_lunch_combo aosp_tian-eng
+add_lunch_combo io_titan-userdebug
+add_lunch_combo io_titan-user
+add_lunch_combo io_titan-eng
